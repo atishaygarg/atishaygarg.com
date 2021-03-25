0e6479127c725334c211ae9c2543bf4c39327489
Atishay Office <atishaygarg03@gmail.com>
2021-03-25 18:04:50 +0530

    Added saving theme preferences

--- script/index.js
+++ script/index.js
@@ -1,7 +1,21 @@
 function darkMode() {
 	document.body.classList.toggle("dark");
+	if (localStorage.getItem('theme') == "dark") {
+		localStorage.setItem('theme', 'light');
+	} 
+	else if (localStorage.getItem('theme') == "light") {
+		localStorage.setItem('theme', 'dark');
+	}
+	else {
+		localStorage.setItem('theme', 'dark');
+	}
 }
 
+function setMode() {
+	if (localStorage.getItem('theme') == 'dark') {
+		document.body.classList.toggle("dark");
+	}
+}
 function test() {
 	alert('Can you read this?')
 }
