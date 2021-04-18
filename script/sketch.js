f1eeac1449125ea8e4a3f9ab419f7a7b6e25a7ad
Atishay Garg <atishaygarg03@gmail.com>
2021-04-18 21:22:20 +0530

    Added main page art using Processing

---
+++ script/sketch.js
@@ -0,0 +1,30 @@
+let theme = localStorage.getItem('theme')
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function pie(colour, w, x, y) {
+ fill(colour);
+ var h = map(mouseY, 0, height, 0, 360);
+ arc(x, y, w, w, radians(0), radians(h));
+}
+
+function draw() {
+  if (theme == "dark") {
+    background(0);
+  }
+  else if (theme == "light") {
+    background(255);
+  }
+  // background(255);
+  fill(100, 10, 20);
+  noStroke();
+  var c = map(mouseX, 0, height, 0, 255);
+  // var col = color(c);
+  for (h = 1; h < 9; h++) {
+    for (w = 1; w < 9; w++) {
+      pie(c, 30, (windowWidth/10)*w, (windowHeight/10)*h);
+    } 
+  }
+}
