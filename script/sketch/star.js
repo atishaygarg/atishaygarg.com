274cc3273a5fcd00ad558882310c228c3b62cd23
Atishay Garg <atishaygarg03@gmail.com>
2021-04-19 08:37:24 +0530

    Added Star Trail

---
+++ script/sketch/star.js
@@ -0,0 +1,44 @@
+function Star() {
+  this.x = random(-width, width);
+  this.y = random(-height, height);
+  this.z = random(width);
+  this.pz = this.z;
+  
+  this.update = function() {
+    this.z = this.z - speed;
+    if (this.z < 1) {
+      this.z = width;
+      this.x = random(-width, width);
+      this.y = random(-height, height);
+      this.pz = this.z;
+    }
+  };
+  this.show = function() {
+    if (theme == "dark") {
+      fill(255);
+    }
+    else if (theme == "light") {
+      fill(0);
+    }
+    noStroke();
+    
+    var sx = map(this.x /this.z, 0, 1, 0, width);
+    var sy = map(this.y /this.z, 0, 1, 0, height);
+    
+    var r = map(this.z, 0, width, 16, 0);
+    ellipse(sx, sy, r, r);
+    
+    var px = map(this.x / this.pz, 0, 1, 0, width);
+    var py = map(this.y / this.pz, 0, 1, 0, height);
+    
+    this.pz = this.z;
+    if (theme == "dark") {
+      stroke(255);
+    }
+    else if (theme == "light") {
+      stroke(0);
+    }
+    // stroke(255);
+    line(px, py, sx, sy);
+  };
+}
---
+++ script/sketch/star_trail.js
@@ -0,0 +1,46 @@
+// Inspired from The Coding Train
+
+let theme = localStorage.getItem('theme')
+let stars = [];
+let speed = 10;
+var tx;
+var ty;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  for (let i=0; i < 800; i++) {
+    stars[i] = new Star();
+  }
+}
+
+
+function draw() {
+  if (theme == "dark") {
+    background(0);
+  }
+  else if (theme == "light") {
+    background(255);
+  }
+  if (!mouseIsPressed) {
+    new_speed = map(mouseX, 0, width, 0, 50);
+    //translate(width/2, height/2);
+  }
+  if (new_speed != null) {
+    speed = new_speed;
+  }
+  //translate(width / 2, height / 2);
+  if (mouseIsPressed) {
+    //translate(mouseX, mouseY);
+    tx = mouseX
+    ty = mouseY
+  }
+  if ((tx == null) && (ty == null)) {
+    tx = width/2;
+    ty = height/2;
+  }
+  translate(tx, ty);
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+}
